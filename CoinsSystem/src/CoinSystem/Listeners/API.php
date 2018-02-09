fcbc45bb0a3a60cbbc751b33ddc367668bb276aa
StuckDexter <pascal.faude@gmail.com>
2018-02-09 17:33:33 +0100

    API

---
+++ CoinsSystem/src/CoinSystem/Listeners/API.php
@@ -0,0 +1,23 @@
+<?php 
+namespace CoinSystem;
+
+class API{
+    
+    public function addPlayer(string $name){
+        
+    }
+    
+    public function getCoins(string $player): int{
+        
+    }
+    
+    public void setCoins(string $player){
+        
+    }
+    
+    public void addCoins(string $player){
+        
+    }
+}
+
+?>
--- CoinsSystem/src/CoinSystem/Listeners/PlayerJoinListener.php
+++ CoinsSystem/src/CoinSystem/Listeners/PlayerJoinListener.php
@@ -1,5 +1,8 @@
 <?php
 
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
 class PlayerJoinListener implements Listener{
     
     public function onJoin(PlayerJoinEvent){
