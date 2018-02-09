5c1f5ac0c6cd925e4df6f9f29d4247adcc64116f
McpeBooster <mcpebooster@gmail.com>
2018-02-09 17:18:36 +0100

    base

--- CoinsSystem/src/CoinSystem/coinsystem.php
+++ CoinsSystem/src/CoinSystem/coinsystem.php
@@ -7,9 +7,8 @@ use pocketmine\plugin\PluginBase;
 class coinsystem extends PluginBase {
 
     const PREFIX = "§7[§eCoinSystem§7] ";
-    
+
     public function onEnable() {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
 
         $this->getLogger()->info(self::PREFIX . "by §6McpeBooster §7and §6StuckDexter§7!");
 
