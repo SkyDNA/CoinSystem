aba68f01b7c000e036cc2525e26cee3faa719247
McpeBooster <mcpebooster@gmail.com>
2018-02-09 17:17:48 +0100

    base

--- CoinsSystem/src/CoinSystem/coinsystem.php
+++ CoinsSystem/src/CoinSystem/coinsystem.php
@@ -1,10 +1,30 @@
 <?php
 namespace CoinSystem;
 
-class coinsystem extends PluginBase{
+use pocketmine\lang\BaseLang;
+use pocketmine\plugin\PluginBase;
 
-public function onEnable(){
+class coinsystem extends PluginBase {
 
+    const PREFIX = "§7[§eCoinSystem§7] ";
+    
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+        $this->getLogger()->info(self::PREFIX . "by §6McpeBooster §7and §6StuckDexter§7!");
+
+        $this->saveDefaultConfig();
+
+        $lang = $this->getConfig()->get("language", BaseLang::FALLBACK_LANGUAGE);
+        $this->baseLang = new BaseLang($lang, $this->getFile() . "resources/");
+
+        $this->getLogger()->info(self::PREFIX . "Language: " . $lang);
+    }
+
+    /**
+     * @return BaseLang
+     */
+    public function getLanguage(): BaseLang {
+        return $this->baseLang;
+    }
 }
-
-?>
