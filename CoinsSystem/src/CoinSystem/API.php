79702692dc85c591c99d36846d9c41e65060badc
StuckDexter <pascal.faude@gmail.com>
2018-02-09 17:35:15 +0100

    APi

---
+++ CoinsSystem/src/CoinSystem/API.php
@@ -0,0 +1,23 @@
+<?php 
+namespace CoinSystem;
+
+class API{
+    
+    public static function addPlayer(string $name){
+        
+    }
+    
+    public static function getCoins(string $player): int{
+        
+    }
+    
+    public static function setCoins(string $player){
+        
+    }
+    
+    public static function addCoins(string $player){
+        
+    }
+}
+
+?>
